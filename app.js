30998a67007fbdfd6b80817af71a259224b492a9
angieappiah <alegnalawn2@yahoo.com>
2021-04-12 22:32:05 -0700

    Added the Model

--- app.js
+++ app.js
@@ -16,5 +16,19 @@ function init() {
     camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.set(-50, 40, 350);
 
+    renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.setSize(container.clientWidth, container.clientHeight)
+    renderer.setPixelRatio(window.devicePixelRatio)
+
+    container.appendChild(renderer.domElement);
+
+
+    let loader = new THREE.GLTFLoader();
+    loader.load("./3d/scene.gltf", function (gltf) {
+        scene.add(gltf.scene)
+
+    })
+
+
 
 }
